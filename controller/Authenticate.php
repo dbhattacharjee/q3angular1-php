2bfec2633bcf386091655ab7a9c7aa2589e30e61
Dipanjan Bhattacharjee <dipanjan.bhattacharjee@myglobalit.com>
2015-12-30 19:17:46 +0530

    Working on read mail feature

--- controller/Authenticate.php
+++ controller/Authenticate.php
@@ -71,22 +71,26 @@ class Authenticate {
                     $mailArray[$key]['dated'] = date('d M, Y H:i', $mail['dated']);
                     $mailArray[$key]['to'] = $mail['toAddress'];
                     $mailArray[$key]['from'] = $mail['fromAddress'];
-                    $mailArray[$key]['mailUID'] = $mail['mailUID'];
+                    $mailArray[$key]['cc'] = $mail['ccAddress'];
+                    $mailArray[$key]['mailUID'] = $mail['mail_uid'];
+                    $mailArray[$key]['mailUuid'] = $mail['mail_uuid'];
                 }
             } else {
                 if ($imap = pop3_login(MAIL_HOST, MAIL_PORT, $username, $password, $folder = "INBOX", MAIL_SSL)) {
                     if($mails = pop3_fetch_emails($imap, LATEST_EMAIL_LIMIT)) {
                         foreach($mails as $key=>$mail) {
-                            $query  = 'INSERT INTO `mails` (user_id, sync_at, `subject`, `toAddress`, `fromAddress`, `dated`, `mail_uid`)';
-                            $query .=  'VALUES (:userId, :syncAt, :subject, :to, :from, :dated, :mailUID)';
+                            $query  = 'INSERT INTO `mails` (user_id, sync_at, `subject`, `toAddress`, `fromAddress`, `dated`, `mail_uid`,  `ccAddress`, `mail_uuid`)';
+                            $query .=  'VALUES (:userId, :syncAt, :subject, :to, :from, :dated, :mailUID, :cc, :mailUuid)';
                             $statement = $conn->prepare($query);
-                            $statement->execute(array(':userId' => $userId, ':syncAt' => time(), ':subject'=>$mail['subject'], ':to'=>$mail['to'], ':from'=>$mail['from'], ':dated'=>$mail['udate'], ':mailUID'=>$mail['uid']));
+                            $statement->execute(array(':userId' => $userId, ':syncAt' => time(), ':subject'=>$mail['subject'], ':to'=>$mail['to'], ':from'=>$mail['from'], ':dated'=>$mail['udate'], ':mailUID'=>$mail['uid'], ':cc'=>$mail['cc'], ':mailUuid'=>$mail['message_id']));
                             
                             $mailArray[$key]['subject'] = $mail['subject'];
                             $mailArray[$key]['dated'] = date('d M, Y H:i', $mail['udate']);
                             $mailArray[$key]['to'] = $mail['to'];
                             $mailArray[$key]['from'] = $mail['from'];
+                            $mailArray[$key]['cc'] = $mail['cc'];
                             $mailArray[$key]['mailUID'] = $mai['uid'];
+                            $mailArray[$key]['mailUuid'] = $mai['message_id'];
                         }
                     }
                 }
@@ -94,6 +98,35 @@ class Authenticate {
         }
 
         return array('status'=>200, 'success'=>true, 'mails'=>$mailArray);
+    }
+    
+    public function getSingleMail($authkey, $uid){
+
+        $auth = explode(':', base64_decode($authkey));
+        $username = $auth[0];
+        $password = $auth[1];
+        
+        if($userId = $this->_validateUser($username, $password)) {
+            //fetch sync mails
+            $conn = $GLOBALS['glob_conn'];
+            $query = 'SELECT * FROM `mails` WHERE `user_id` = :user_id AND `mail_uid` = :uid AND `body` IS NOT NULL';
+            $statement = $conn->prepare($query);
+            $statement->execute(array(':user_id' => trim($userId), ':uid'=>$uid));
+            $body = '';
+            if(!$mail = $statement->fetch()) {
+                //fetch email
+                $imap = pop3_login(MAIL_HOST, MAIL_PORT, $username, $password, $folder = "INBOX", MAIL_SSL);
+                $body = pop3_get_body($uid, $imap);
+                //update
+                $query = 'UPDATE `mails` SET body = :body WHERE `user_id` = :user_id AND `mail_uid` = :uid';
+                $statement = $conn->prepare($query);
+                $statement->execute(array(':user_id' => trim($userId), ':uid'=>$uid, ':body'=>$body));
+            } else {
+                $body = $mail['body'];
+            }
+        }
+
+        return array('status'=>200, 'success'=>true, 'mailBody'=>$body);
     }
 
 }
--- library/pop3.php
+++ library/pop3.php
@@ -30,71 +30,72 @@ function pop3_dele($connection,$message)
 {
     return(imap_delete($connection,$message));
 }
-function mail_parse_headers($headers)
-{
-    $headers=preg_replace('/\r\n\s+/m', '',$headers);
-    preg_match_all('/([^: ]+): (.+?(?:\r\n\s(?:.+?))*)?\r\n/m', $headers, $matches);
-    foreach ($matches[1] as $key =>$value) $result[$value]=$matches[2][$key];
-    return($result);
-}
-function mail_mime_to_array($imap,$mid,$parse_headers=false)
-{
-    $mail = imap_fetchstructure($imap,$mid);
-    $mail = mail_get_parts($imap,$mid,$mail,0);
-    if ($parse_headers) $mail[0]["parsed"]=mail_parse_headers($mail[0]["data"]);
-    return($mail);
-}
-function mail_get_parts($imap,$mid,$part,$prefix)
-{   
-    $attachments=array();
-    $attachments[$prefix]=mail_decode_part($imap,$mid,$part,$prefix);
-    if (isset($part->parts)) // multipart
-    {
-        $prefix = ($prefix == "0")?"":"$prefix.";
-        foreach ($part->parts as $number=>$subpart)
-            $attachments=array_merge($attachments, mail_get_parts($imap,$mid,$subpart,$prefix.($number+1)));
-    }
-    return $attachments;
-}
-function mail_decode_part($connection,$message_number,$part,$prefix)
-{
-    $attachment = array();
-
-    if($part->ifdparameters) {
-        foreach($part->dparameters as $object) {
-            $attachment[strtolower($object->attribute)]=$object->value;
-            if(strtolower($object->attribute) == 'filename') {
-                $attachment['is_attachment'] = true;
-                $attachment['filename'] = $object->value;
-            }
-        }
-    }
-
-    if($part->ifparameters) {
-        foreach($part->parameters as $object) {
-            $attachment[strtolower($object->attribute)]=$object->value;
-            if(strtolower($object->attribute) == 'name') {
-                $attachment['is_attachment'] = true;
-                $attachment['name'] = $object->value;
-            }
-        }
-    }
-
-    $attachment['data'] = imap_fetchbody($connection, $message_number, $prefix);
-    if($part->encoding == 3) { // 3 = BASE64
-        $attachment['data'] = base64_decode($attachment['data']);
-    }
-    elseif($part->encoding == 4) { // 4 = QUOTED-PRINTABLE
-        $attachment['data'] = quoted_printable_decode($attachment['data']);
-    }
-    return($attachment);
-}
+//function mail_parse_headers($headers)
+//{
+//    $headers=preg_replace('/\r\n\s+/m', '',$headers);
+//    preg_match_all('/([^: ]+): (.+?(?:\r\n\s(?:.+?))*)?\r\n/m', $headers, $matches);
+//    foreach ($matches[1] as $key =>$value) $result[$value]=$matches[2][$key];
+//    return($result);
+//}
+//function mail_mime_to_array($imap,$mid,$parse_headers=false)
+//{
+//    $mail = imap_fetchstructure($imap,$mid);
+//    $mail = mail_get_parts($imap,$mid,$mail,0);
+//    if ($parse_headers) $mail[0]["parsed"]=mail_parse_headers($mail[0]["data"]);
+//    return($mail);
+//}
+//function mail_get_parts($imap,$mid,$part,$prefix)
+//{   
+//    $attachments=array();
+//    $attachments[$prefix]=mail_decode_part($imap,$mid,$part,$prefix);
+//    if (isset($part->parts)) // multipart
+//    {
+//        $prefix = ($prefix == "0")?"":"$prefix.";
+//        foreach ($part->parts as $number=>$subpart)
+//            $attachments=array_merge($attachments, mail_get_parts($imap,$mid,$subpart,$prefix.($number+1)));
+//    }
+//    return $attachments;
+//}
+//function mail_decode_part($connection,$message_number,$part,$prefix)
+//{
+//    $attachment = array();
+//
+//    if($part->ifdparameters) {
+//        foreach($part->dparameters as $object) {
+//            $attachment[strtolower($object->attribute)]=$object->value;
+//            if(strtolower($object->attribute) == 'filename') {
+//                $attachment['is_attachment'] = true;
+//                $attachment['filename'] = $object->value;
+//            }
+//        }
+//    }
+//
+//    if($part->ifparameters) {
+//        foreach($part->parameters as $object) {
+//            $attachment[strtolower($object->attribute)]=$object->value;
+//            if(strtolower($object->attribute) == 'name') {
+//                $attachment['is_attachment'] = true;
+//                $attachment['name'] = $object->value;
+//            }
+//        }
+//    }
+//
+//    $attachment['data'] = imap_fetchbody($connection, $message_number, $prefix);
+//    if($part->encoding == 3) { // 3 = BASE64
+//        $attachment['data'] = base64_decode($attachment['data']);
+//    }
+//    elseif($part->encoding == 4) { // 4 = QUOTED-PRINTABLE
+//        $attachment['data'] = quoted_printable_decode($attachment['data']);
+//    }
+//    return($attachment);
+//}
 
 function pop3_fetch_emails($imap, $limit = 20) {
     $numMessages = imap_num_msg($imap);
     $details = array();
     for ($i = $numMessages; $i > ($numMessages - $limit); $i--) {
         $header = imap_header($imap, $i);
+        
 
         $from = $header->from;
         $fromInfo = '';
@@ -124,16 +125,89 @@ function pop3_fetch_emails($imap, $limit = 20) {
             }
             $toInfo .= $temp;
         }
+        $ccInfo = '';
+        if (property_exists($header, 'cc')) {
+            $cc = $header->cc;
+            $ccInfo = '';
+            foreach ($cc as $value) {
+                if ($ccInfo != '') {
+                    $ccInfo .= ', ';
+                }
+                $temp = $value->mailbox . '@' . $value->host;
+                if (property_exists($value, 'personal')) {
+                    $temp = '<a href="mailto:' . $temp . '" target="_top">' . $value->personal . '</a>';
+                } else {
+                    $temp = '<a href="mailto:' . $temp . '" target="_top">' . $temp . '</a>';
+                }
+                $ccInfo .= $temp;
+            }
+        }
+        
         $fromInfo = str_replace("'", "", $fromInfo);
         $toInfo = str_replace("'", "", $toInfo);
+        $ccInfo = str_replace("'", "", $ccInfo);
 
         $details[] = array(
             "from" => $fromInfo,
             "to" => $toInfo,
+            "cc" => $ccInfo,
             "subject" => (isset($header->subject)) ? trim($header->subject) : "",
             "udate" => (isset($header->udate)) ? $header->udate : time(),
-            "uid"=>imap_uid($imap, $i)
+            "uid"=>imap_uid($imap, $i),
+            "message_id"=>$header->message_id
         );
     }
     return $details;
 }
+
+function pop3_get_body($uid, $imap) {
+    $body = get_part($imap, $uid, "TEXT/HTML");
+    // if HTML body is empty, try getting text body
+    if ($body == "") {
+        $body = get_part($imap, $uid, "TEXT/PLAIN");
+    }
+    return $body;
+}
+
+function get_part($imap, $uid, $mimetype, $structure = false, $partNumber = false) {
+    if (!$structure) {
+           $structure = imap_fetchstructure($imap, $uid, FT_UID);
+    }
+    if ($structure) {
+        if ($mimetype == get_mime_type($structure)) {
+            if (!$partNumber) {
+                $partNumber = 1;
+            }
+            $text = imap_fetchbody($imap, $uid, $partNumber, FT_UID);
+            switch ($structure->encoding) {
+                case 3: return imap_base64($text);
+                case 4: return imap_qprint($text);
+                default: return $text;
+           }
+       }
+
+        // multipart 
+        if ($structure->type == 1) {
+            foreach ($structure->parts as $index => $subStruct) {
+                $prefix = "";
+                if ($partNumber) {
+                    $prefix = $partNumber . ".";
+                }
+                $data = get_part($imap, $uid, $mimetype, $subStruct, $prefix . ($index + 1));
+                if ($data) {
+                    return $data;
+                }
+            }
+        }
+    }
+    return false;
+}
+
+function get_mime_type($structure) {
+    $primaryMimetype = array("TEXT", "MULTIPART", "MESSAGE", "APPLICATION", "AUDIO", "IMAGE", "VIDEO", "OTHER");
+
+    if ($structure->subtype) {
+       return $primaryMimetype[(int)$structure->type] . "/" . $structure->subtype;
+    }
+    return "TEXT/PLAIN";
+}   
